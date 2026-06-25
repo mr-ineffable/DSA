770051fae799c1ddfbbc2c3e6d9eaddd2bee8299
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-06-25 22:25:15 +0530

    Tried Factorial, GCD, Trailing zeroes in factorial

--- GFG/mathematics.cpp
+++ GFG/mathematics.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
 using namespace std;
+using namespace boost::multiprecision;
+
 int countDigits(int n){
     int cnt = 0;
     while(n>0){
@@ -20,14 +23,55 @@ bool checkPalindrome(int n){
     }
     return false;
 }
+long long findFactorial(int n){
+    if(n<0) return -1;
+    long long factorial = 1;
+    for(int i=1;i<=n;i++){
+        factorial *= i;
+    }
+    return factorial;
+}
+cpp_int biggerFactorial(long long n){
+    if (n < 0) return -1;
+    cpp_int factorial = 1;
+    for(int i=1; i<=n;i++){
+        factorial *=i;
+    }
+    return factorial;
+}
+
+int countTrailingZeroes(long long n){
+    long long var = 5;
+    int trailingZeroes = 0;
+    while(n/var >0){
+        trailingZeroes += n/var;
+        var *=5;
+    }
+    return trailingZeroes;
+}
+long long gcd(long long a, long long b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b, a%b);
+}
 int main(){
-    int digits = countDigits(54321);
-    cout << digits << endl;
     int number = 98789;
+    int digits = countDigits(number);
+    cout <<"No of digits in "<< number << " are : " << digits << endl;
+    
     if(checkPalindrome(number)){
-        cout <<"This is palindrome";
+        cout <<"This is palindrome\n";
     }
     else{
-        cout <<"This is not a palindrome";
+        cout <<"This is not a palindrome\n";
     }
+    number = 20;
+    cout << "Factorial of number " << number << " is :" << findFactorial(number) << endl;
+
+    // number = 7689;
+    // cout <<"Factorial of a bigger number " << number << " is :" << biggerFactorial(number) << endl;
+    cout <<"Trailing Zeroes for the factorial of "<<number<< " are :" << countTrailingZeroes(number)<< endl;
+    cout <<"Trailing Zeroes for the factorial of "<<251<< " are :" << countTrailingZeroes(251)<< endl;
+    cout <<"GCD of "<< 12 << " and " << 15 <<" is " << gcd(12,15) << endl;
 }
