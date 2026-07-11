83273d372fe30658a77e6caf953c0bd8caa6a342
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-07-11 10:15:48 +0530

    long stores 4 bytes on windows and 8 bytes on linux

--- Codeforces/A2230.cpp
+++ Codeforces/A2230.cpp
@@ -4,11 +4,11 @@ int main(){
     int tt;
     cin >> tt;
     while(tt--){
-        long n, a ,b;
+        long long n, a ,b;
         cin >> n >> a >> b;
-        long long ans1 = (n / 3) * min(b, 3 * a) + (n % 3) * a;
-        long long ans2 = ((n + 2) / 3) * min(b, 3 * a);
-        cout << min(ans1, ans2) << '\n';
+        long long  threeIndividually = 3 * a;
+        long long minForThree = min(threeIndividually, b);
+        cout << (n/3)*minForThree + min((n%3)*a, b) << endl;
         
     }
 }
