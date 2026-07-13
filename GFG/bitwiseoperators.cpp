850a9eaa1da6945af5d3fd51064c92cd793a5f9f
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-07-13 22:21:27 +0530

    Tried more bitwise operators, Learned about 2's and 1's complement

--- GFG/bitwiseoperators.cpp
+++ GFG/bitwiseoperators.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main(){
     cout << "Bitwise and operator " << 1 <<" & " << 2 << ": " << (1 & 2) << endl;
     cout << "Bitwise or operator " << 1 << "|" << 2 <<" : " <<(1 | 2) << endl;
-    cout << "Bitwise xor operator " << 1 << "|" << 2 <<" : " <<(1 ^ 2) << endl;
+    cout << "Bitwise xor operator " << 1 << "^" << 2 <<" : " <<(1 ^ 2) << endl;
+    cout << "Bitwise >>(Right shift) operator " << (5>>1) << endl;
+    cout << "Bitwise <<(Left shift) operator " << (5<<1) << endl;
+    //bitwise Not only unary operator
+    cout << "Bitwise ~ operator" << ~5 << endl;
 }
