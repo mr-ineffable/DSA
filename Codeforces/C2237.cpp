2f87d54ec614e8e487c6020c81eee050add3a2be
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-07-21 18:30:11 +0530

    fixed the logic issue with div 1 problem yesterday

--- Codeforces/C2237.cpp
+++ Codeforces/C2237.cpp
@@ -8,10 +8,10 @@ int main(){
         cin >> n;
         vector<long long>v(n);
         for(int i=0;i<n;i++) cin >> v[i];
-        for(int i =1;i<n-1;i++){
-            if(v[i]>v[i-1]){
-                swap(v[i],v[i+1]);
-                v[i+1] += v[i];
+        for(int i =1;i<n;i++){
+            if(v[i]<v[i-1]){
+                swap(v[i],v[i-1]);
+                v[i] += v[i-1];
             }
         }
         cout << v[v.size()-1] << endl;
