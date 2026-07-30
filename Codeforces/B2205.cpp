397311ed95c814ad7dcdfac99f8572148aeafc12
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-07-30 16:04:02 +0530

    solved and found the edge cases of yesterday's div 2 problem

--- Codeforces/B2205.cpp
+++ Codeforces/B2205.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isPrime(int n){
-    if(n==1) return false;
-    if(n==2||n==3) return true;
-    if(n%2==0 || n%3==0) return false;
-    int sqrtofn = sqrt(n);
-    for(int i =5; i <= sqrtofn; i+=6){
-        if(n%i==0 || n%(i+2)==0) return false;
+bool isPrime(long long n){
+    if(n<=1) return false;
+    else if(n==2||n==3) return true;
+    else if(n%2==0||n%3==0) return false;
+    for(long i=5; i <=sqrtl(n); i+=6){
+        if(n%i==0|| n%(i+2)==0) return false;
     }
     return true;
 }
@@ -16,25 +15,22 @@ int main(){
     while(tt--){
         long long n;
         cin >> n;
-        vector<long long> divisorsOfN;
-        long long sqrtOfN = sqrtl(n);
-        for (long long i = 2; i <= sqrtOfN; i++) {
-            if (n % i == 0) {
-                if(isPrime(i)) divisorsOfN.push_back(i);
-        
-                long long second = n / i;
-                // cout << second <<" " << isPrime(second) << endl;
-                if (second != i && isPrime(second))
-                    divisorsOfN.push_back(second);
+        if(isPrime(n)) cout << n << endl;
+        else{
+            vector<int>UniquePrimeDivisorsOfN;
+            for(int i = 2; i <= sqrtl(n);i++){
+                //divisors they always lie in pair
+                //if first divisor is i then find second divisor
+                int secondDivisor = n / i;
+                if(n%i==0 && isPrime(i)) UniquePrimeDivisorsOfN.push_back(i);
+                if(n%secondDivisor==0 && isPrime(secondDivisor) && i != secondDivisor) UniquePrimeDivisorsOfN.push_back(secondDivisor);
             }
+            long long ans = 1;
+            int sz = UniquePrimeDivisorsOfN.size();
+            for(int i =0;i<sz ; i++){
+                ans *= UniquePrimeDivisorsOfN[i];
+            }
+            cout << ans <<"\n";
         }
-        long long ans = 1;
-        int sz = divisorsOfN.size();
-        for(int i=0;i<sz; i++){
-            // cout << divisorsOfN[i] << endl;
-            ans *= divisorsOfN[i];
-        }
-        cout << ans << "\n";
-        
     }
 }
