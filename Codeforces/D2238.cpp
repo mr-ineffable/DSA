ecd90098c3cc17017673e5a0f39fa7b4c4b89f0e
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-08-04 19:37:52 +0530

    Went through tutorial understood the concept and solved the question

--- Codeforces/D2238.cpp
+++ Codeforces/D2238.cpp
@@ -1,18 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int gcdOfAAndB(int A, int B){
-    if(B==0) return A;
-    return gcdOfAAndB(B, A%B);
-}
 bool isPrime(int n){
-    if(n ==1) return false;
-    else if(n==2||n==3) return true;
-    else if(n%2==0||n%3==0) return false;
-    for(int i = 5 ; i <= sqrt(n); i+=6){
-        if(n%i ==0 || n%(i+2)==0) return false;
+    if(n==1) return false;
+    if(n==2||n==3) return true;
+    if(n%2==0||n%3==0) return false;
+    for(int i = 5; i <=sqrt(n);i+=6){
+        if(n%i==0||n%(i+2)==0) return false;
     }
     return true;
-    
 }
 int main(){
     int tt;
@@ -20,44 +15,24 @@ int main(){
     while(tt--){
         int n;
         cin >> n;
+        unordered_map<int,int>mp;
+        if(isPrime(n)) mp[n] = 1;
         int squareRoot = sqrt(n);
-        vector<int>divisorsOfN;
-        for(int i = 2 ; i <= squareRoot; i++){
-            if(n%i == 0){
-                divisorsOfN.push_back(i);
-                int secondNumber = n/i;
-                if(secondNumber != i){
-                    divisorsOfN.push_back(secondNumber);
-                }
+        for(int i = 2; i <=squareRoot; i++){
+            while(isPrime(i) && n%i==0){
+                mp[i]++;
+                n /= i;
             }
+            
         }
-        sort(divisorsOfN.begin(),divisorsOfN.end());
-        divisorsOfN.push_back(n);
-        int layer  = 0;
-        int sz = divisorsOfN.size()-1;
-        int l = 0;
-        int lastNumber = 0; 
-        while(l <= sz){
-            if(isPrime(divisorsOfN[l])|| l==sz){
-                layer++;
-                l++;
-            }
-            else{
-                if(gcdOfAAndB(lastNumber,divisorsOfN[l])!=1){
-                    layer++;
-                    lastNumber= divisorsOfN[l];
-                }
-                else{
-                    lastNumber = 0;
-                }
-                l++;
-            }
+        if(n!=1) mp[n] = 1;
+        int A = 0,m=0;
+        for(auto & i :mp){
+            m++;
+            A+=i.second;
+            // cout << i.first <<" " << i.second << endl;
         }
-        // for(int i = 0 ; i < sz ;i++){
-        //     cout << divisorsOfN[i] <<" " ;
-        // }cout << endl;
-        
-        cout << layer << endl;
+        cout << A+m-1 << endl;
         
     }
 }
