01d3ce276f257a9457ea5687170cc4ff910a42f2
mr-ineffable <priyanshusharma23052002@gmail.com>
2026-08-08 20:55:11 +0530

    solved the yesterday bug and solved the question

--- Codeforces/A2248.cpp
+++ Codeforces/A2248.cpp
@@ -1,26 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+int firstOccurenceOfZero(string &s){
+    int sz = s.size();
+    for(int i = 0 ; i < sz; i++ ){
+        if(s[i]=='0') return i;
+    }
+    return -1;
+}
+int firstOccurenceOfOne(string &s){
+    int sz = s.size();
+    for(int i = 0; i < sz; i++){
+        if(s[i]=='1') return i;
+    }
+    return -1;
+}
 int main(){
     int tt;
     cin >> tt;
     while(tt--){
         string s;
         cin >> s;
-        int sz = sz.size();
-        int firstOccurenceOfOne = -1,firstOccurenceOfZero=-1;
-        for(int i=0; i < sz; i++ ){
-            if(firstOccurenceOfOne ==-1 && s[i]=='1'){
-                firstOccurenceOfOne = i;
-            }
-            else if(firstOccurenceOfOne != -1 && s[i]=='0'){
-                firstOccurenceOfZero = i;
-            }
-        }
-        if(firstOccurenceOfZero == -1) firstOccurenceOfZero = 0;
+        int sz = s.size();
+        int o = firstOccurenceOfOne(s);
+        int z = firstOccurenceOfZero(s);
         string newString = "";
-        for(int i=0; i < sz; i++){
-            if(i!=firstOccurenceOfZero && i!= firstOccurenceOfOne ) newString += s[i];
+        for(int i = 0 ; i < sz; i++){
+            if(i== o || i== z) {}
+            else newString += s[i];
         }
         cout << newString << "\n";
+        
     }
 }
